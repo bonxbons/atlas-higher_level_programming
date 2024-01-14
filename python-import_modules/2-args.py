7aecba39b9cec59cd733c3f174406078002d07f4
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2024-01-14 04:21:00 -0600

    Update 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 from sys import argv
+if __name__ == "__main__":
+    num = len(argv)
 
-if len(argv) == 1:
-    print("0 arguments.")
-elif len(argv) == 2:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(len(argv) - 1))
+    if num < 2:
+        print("0 arguments.")
 
-for i, arg in enumerate(argv[1:], 1):
-    print("{}: {}".format(i, arg))
+    elif num == 2:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+
+    else:
+        print("{} arguments:".format(num - 1))
+
+        for i in range(1, num):
+            print("{0}: {1}".format(i, argv[i]))
