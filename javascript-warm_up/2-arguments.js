b6dc35294e6fd5b4194df33511b3905277539298
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2024-03-26 19:09:03 -0500

    Update 2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
-
-if (process.argv.length > 3) {
-  console.log('Arguments found');
-} else if (process.argv.length === 3) {
-  console.log('Argument found');
-} else {
-  console.log('No argument');
+// If no arguments are passed to the script, print “No argument”
+if (process.argv.length === 2) {
+  console.log("No argument");
+}
+// If only one argument is passed to the script, print “Argument found”
+else if (process.argv.length === 3) {
+  console.log("Argument found");
+}
+// Otherwise, print “Arguments found”
+else {
+  console.log("Arguments found");
 }
