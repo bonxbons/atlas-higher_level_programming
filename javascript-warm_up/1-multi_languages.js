d493cc1f5ddaf63724b4d2693c4cd4459480b371
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2024-03-26 19:08:20 -0500

    Update 1-multi_languages.js

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-// First line: “C is fun”
-console.log("C is fun");
-// Second line: “Python is cool”
-console.log("Python is cool");
-// Third line: “JavaScript is amazing”
-console.log("JavaScript is amazing");
+// First line: 'C is fun'
+console.log('C is fun');
+// Second line: 'Python is cool'
+console.log('Python is cool');
+// Third line: 'JavaScript is amazing'
+console.log('JavaScript is amazing');
