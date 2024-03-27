6ee8ff7a822351bac5d650ad3433a830da0b5259
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2024-03-26 19:29:31 -0500

    Update 4-concat.js

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -2,12 +2,8 @@
 // If at least two arguments are passed to the script, print two arguments passed to it, in the format: ' is '
 if (process.argv.length >= 4) {
   console.log(process.argv[2] + ' is ' + process.argv[3]);
-}
-// If only one argument is passed to the script, print the first argument followed by " is undefined"
-else if (process.argv.length === 3) {
+} else if (process.argv.length === 3) { // If only one argument is passed to the script, print the first argument followed by " is undefined"
   console.log(process.argv[2] + ' is undefined');
-}
-// If no arguments are passed to the script, print "undefined is undefined"
-else {
+} else { // If no arguments are passed to the script, print "undefined is undefined"
   console.log('undefined is undefined');
 }
