844a2a72328059dea4e9e3333d54fa53c86df529
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2024-03-26 19:12:25 -0500

    Update 3-value_argument.js

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
-
-if (process.argv[2]) {
+// If no arguments are passed to the script, print “No argument”
+if (process.argv.length === 2) {
+  console.log("No argument");
+}
+// Otherwise, print the first argument passed to the script
+else {
   console.log(process.argv[2]);
-} else {
-  console.log('No argument');
 }
