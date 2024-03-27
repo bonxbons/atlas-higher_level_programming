c3c2f622357aa0853e6b3e3f0be6b162c4288ff7
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2024-03-26 19:41:28 -0500

    Update 9-add.js

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,15 +1,7 @@
 #!/usr/bin/node
 
 function add(a, b) {
-  return a + b;
+  return parseInt(a) + parseInt(b);
 }
 
-const num1 = parseInt(process.argv[2]);
-const num2 = parseInt(process.argv[3]);
-
-if (!isNaN(num1) && !isNaN(num2)) {
-  console.log(add(num1, num2));
-} else {
-  console.log("NaN");
-}
-
+console.log(add(process.argv[2], process.argv[3]));
