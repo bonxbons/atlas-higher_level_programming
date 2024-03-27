1214e135a15be08a6ea6c3667385a53986d5198d
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2024-03-27 10:10:22 -0500

    Update 1-rectangle.js

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
+// 1-rectangle.js
 class Rectangle {
-    constructor(w, h) {
-      this.width = w;
-      this.height = h;
-    }
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
   }
-  
-  module.exports = Rectangle;
-  
+}
+
+module.exports = Rectangle;
